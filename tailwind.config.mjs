9bd2947efebd7eb0bd96a323a1c230dce5b01e5a
kidboi666 <kidboi666@gmail.com>
2024-11-20 14:00:02 +0900

    design : 전체적인 레이아웃 잡음

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const addUtilities = ({ addUtilities }) => {
+const pluginContainer = ({ addUtilities }) => {
   addUtilities({
     ".scrollbar-none": {
       "-ms-overflow-style": "none", // IE and Edge
@@ -14,8 +14,19 @@ const addUtilities = ({ addUtilities }) => {
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          from: { opacity: "0", transform: "translateY(8px)" },
+          to: { opacity: "1", trnasform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-in-out",
+      },
+    },
   },
-  plugins: [addUtilities],
+  plugins: [pluginContainer],
 };
