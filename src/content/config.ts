86375fd5e47597b87e6ba4f008d747fb2619381e
kidboi666 <kidboi666@gmail.com>
2024-11-20 18:03:24 +0900

    docs : 컨텐츠 추가

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,10 @@ export const categories = [
   "JavaScript",
   "TypeScript",
   "React",
+  "NextJS",
   "Axios",
+  "Web",
+  "Redux",
 ] as const;
 
 const HTML = defineCollection({
@@ -62,12 +65,42 @@ const TypeScript = defineCollection({
     category: z.string(),
   }),
 });
+const NextJS = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    pubDate: z.date(),
+    category: z.string(),
+  }),
+});
+const Web = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    pubDate: z.date(),
+    category: z.string(),
+  }),
+});
+const Redux = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    pubDate: z.date(),
+    category: z.string(),
+  }),
+});
 
 export const collections = {
   HTML,
   CSS,
+  NextJS,
   React,
   JavaScript,
   TypeScript,
   Axios,
+  Web,
+  Redux,
 };
