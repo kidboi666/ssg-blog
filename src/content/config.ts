0db7ca051b99688800669064f744aa253ee858bc
kidboi666 <kidboi666@gmail.com>
2024-11-20 15:53:44 +0900

    docs : 컨텐츠 추가

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,12 @@
 import { defineCollection, z } from "astro:content";
+export const categories = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Axios",
+] as const;
 
 const HTML = defineCollection({
   type: "content",
@@ -36,10 +44,30 @@ const JavaScript = defineCollection({
     category: z.string(),
   }),
 });
+const Axios = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    pubDate: z.date(),
+    category: z.string(),
+  }),
+});
+const TypeScript = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    pubDate: z.date(),
+    category: z.string(),
+  }),
+});
 
 export const collections = {
   HTML,
   CSS,
   React,
   JavaScript,
+  TypeScript,
+  Axios,
 };
