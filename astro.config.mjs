55d4fcfc5060b6a431a5e70fb579acc9bbd36f14
kidboi666 <kidboi666@gmail.com>
2024-11-21 12:19:33 +0900

    feat : mermaid 추가 설치

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
+import remarkMermaid from "remark-mermaidjs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMermaid],
+  },
 });
