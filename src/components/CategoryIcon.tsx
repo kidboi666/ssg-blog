7912e78df3fbf2f5c170bb66d83f5bdeae88ea97
kidboi666 <kidboi666@gmail.com>
2024-11-22 18:37:27 +0900

    fix : 카테고리 아이콘 이미지 불러오는 방식 변경

--- src/components/CategoryIcon.tsx
+++ src/components/CategoryIcon.tsx
@@ -1,29 +1,32 @@
-const categoryIcons = {
-  NextJS:
-    "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566879300/noticon/fvty9lnsbjol5lq9u3by.svg",
-  React:
-    "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566557331/noticon/d5hqar2idkoefh6fjtpu.png",
-  JavaScript:
-    "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1570946287/noticon/qgdiv5ctkcneujidjuv1.png",
-  TypeScript:
-    "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566913457/noticon/eh4d0dnic4n1neth3fui.png",
-  CSS: "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566912109/noticon/puksfce6wca36hes1vom.png",
-  HTML: "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566995514/noticon/jufppyr8htislboas4ve.png",
-  Axios:
-    "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1677561097/noticon/ea5cajzpklxgucnisgy4.png",
-  Web: "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1590043914/noticon/xe5nasyjil6mn6vk8c4s.png",
-  Redux:
-    "https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1567749614/noticon/zgdaxpaif5ojeduonygb.png",
-  Astro: "../assets/icons/astro.svg",
-};
+import HTML from "../assets/icons/html.png";
+import CSS from "../assets/icons/css.png";
+import JavaScript from "../assets/icons/js.png";
+import TypeScript from "../assets/icons/ts.png";
+import Astro from "../assets/icons/astro.svg";
+import React from "../assets/icons/react.png";
+import NextJS from "../assets/icons/next.svg";
+import Axios from "../assets/icons/axios.png";
+import Web from "../assets/icons/web.png";
 
 interface Props {
   category: keyof typeof categoryIcons;
 }
 
+const categoryIcons = {
+  HTML,
+  CSS,
+  JavaScript,
+  TypeScript,
+  Astro,
+  React,
+  NextJS,
+  Axios,
+  Web,
+};
+
 const CategoryIcon = ({ category }: Props) => {
   const imgSrc = categoryIcons[category];
-  return <img width={24} height={24} src={imgSrc} className="rounded-md" />;
+  return <img width={24} height={24} src={imgSrc.src} className="rounded-md" />;
 };
 
 export default CategoryIcon;
